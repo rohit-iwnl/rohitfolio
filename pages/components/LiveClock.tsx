185c4cfb5f62546e12a7d1d93b9213e2000b9dc6
Rohit Manivel <rohitmanivel9@gmail.com>
2024-01-10 17:51:18 -0700

    fix timezone

--- pages/components/LiveClock.tsx
+++ pages/components/LiveClock.tsx
@@ -4,5 +4,5 @@ import Clock from "react-live-clock";
 type Props = {};
 
 export default function LiveClock({}: Props) {
-  return <Clock format={"h:mm:ss a"} ticking={true} timezone={"US/Phoenix"} className="text-[4.5vw] md:text-[3vw] lg:text-[2vw] text-accent"/>;
+  return <Clock format={"h:mm:ss a"} ticking={true} timezone={"America/Phoenix"} className="text-[4.5vw] md:text-[3vw] lg:text-[2vw] text-accent"/>;
 }
