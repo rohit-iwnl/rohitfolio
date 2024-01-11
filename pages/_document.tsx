f5dfe63a1a659ed0cd07acf819e02d9d7b567bfc
Rohit Manivel <rohitmanivel9@gmail.com>
2024-01-10 17:48:25 -0700

    optimise for pagespeed insights

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Rohitfolio</title>
+      <meta name="description" content="Rohit's Portfolio" />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
