bd42fa06e46a4a4bcaa6a670750248ce613aeaf9
Rohit Manivel <rohitmanivel9@gmail.com>
2024-01-15 12:42:52 -0700

    seo fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Analytics } from '@vercel/analytics/react';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Rohitfolio</title>
+        <meta name="description" content="Rohit's Portfolio" />
+        <meta name="author" content="Rohit Manivel" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,11 @@
 
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html,Head,Main, NextScript } from "next/document";
+
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <title>Rohitfolio</title>
-      <meta name="description" content="Rohit's Portfolio" />
-      <meta name="author" content="Rohit Manivel" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body>
         <Main />
         <NextScript />
